27e419aa21b2e777a596c2f0aade8afbd17f7310
cmkm3010 <cmkm8384@gmail.com>
2023-10-19 15:21:07 +0900

    ㅑㅜㅑㅅ

---
+++ A-1/js/script.js
@@ -0,0 +1,55 @@
+$(function(){
+
+    // 모달
+    $('.show_modal').click(function(){
+        $('.modal').show();
+        });
+
+    $('.close_modal').click(function(){
+    $('.modal').hide();
+     });
+
+//     // $('.gnb').hover(function(){
+//     //     $('.gnb ul').slideDown(600);
+//     // }),
+//     // $('.gnb').hover(function(){
+//     //     $('.gnb ul').slideUp(600);
+//     // })
+
+//     setInterval(function(){
+//         $('.slides').animate({left:'-100%'},function(){
+//             $('.slide').first().appendTo('.slides')
+//             $(this).css('top',0)
+//         })
+//     },3000)
+
+//     $('.items').eq(0).show()
+
+//     $('.titles h2').click(function(){
+//         $('.titles h2').removeClass('active')
+//         $(this).addClass('active')
+//         var index =$(this).index 
+//         console.log(index)
+
+//         $('.itmes').hide()
+//         $('.itmes').eq()
+//     })
+    
+
+
+// })
+
+// $('.show_modal').click(function(){
+//     $('.modal').show();
+// });
+
+// $('.close_modal').click(function(){
+//     $('.modal').hide();
+// });
+
+// $('.gnb').hover(function(){
+//     $('.gnb ul').stop().slideDown(600);
+// }),
+// $('.gnb').hover(function(){
+//     $('.gnb ul').stop().slideUp(600);
+})
