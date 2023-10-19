a1cb790eeef0df691108b081581c6849c3c7212b
cmkm3010 <cmkm8384@gmail.com>
2023-10-19 17:17:32 +0900

    init

--- 20231019 A-1/js/main.js
+++ 20231019 A-1/js/main.js
@@ -1,3 +1,23 @@
 $(function(){
-    //
+
+    // modal
+    $('.show_modal').click(function(){
+        $('.modal').show()
+    });
+
+    $('.close').click(function(){
+        $('.modal').hide()
+    });
+
+    // gnb
+   $('.gnb').hover(
+        function(){
+            $(this).find('ul').stop().slideDown()
+   }),
+   $('.gnb').hover(
+        function(){
+            $(this).find('ul').stop().slideUp()
+   })
+    
+
 })
