f34e0aae44587cda2c41d862083ac78f7006c9ca
cmkm3010 <cmkm8384@gmail.com>
2023-10-20 15:01:01 +0900

    init

--- 20231019 A-1/js/main.js
+++ 20231019 A-1/js/main.js
@@ -10,6 +10,8 @@ $(function(){
     });
 
     // gnb
+
+    // 1. submenu 전체 내림
    $('.gnb').hover(
         function(){
             $(this).find('ul').stop().slideDown()
@@ -19,9 +21,9 @@ $(function(){
         },
         )   
 
-
     // slider
     
+    // 1) 이미지 위 -> 아래
     // setInterval(function(){
     //     $('.slides').animate({top:'-100%'},function(){
     //         $('.slide').first().appendTo('.slides')
@@ -29,12 +31,27 @@ $(function(){
     //     })
     // },3000)
 
+    // 2) 이미지 좌 -> 우
     setInterval(function(){
         $('.slides').animate({left: '-100%'}, function(){
             $('.slide').first().appendTo('.slides')
             $('.slides').css('left', 0)
         })
     }, 3000)
+
+    //board
+    $('.item').eq(0).show()
+
+    $('.titles h2').click(function(){
+        var index =$(this).index()
+
+        $('.titles h2').removeClass('active')
+        $(this).addClass('active')
+
+        $('.item').hide()
+        $('.item').eq(index).show()
+    })
+
 
 
 
